64d747125fe0a3cf86a0cc0a87178ec03da5156c
26oglel <144705413+26oglel@users.noreply.github.com>
2023-09-25 10:41:26 -0400

    script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,13 +7,18 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.D))
-        { }
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Debug.Log("moveHorizontal:" + moveHorizontal);
+
+        float moveVertical = Input.GetAxis("Vertical");
+        //   if (Input.GetKeyDown(KeyCode.D))
+        //{ 
+        //}
     }
 }
