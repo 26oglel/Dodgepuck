bd1f3f316f3f2b8fadca51069d1d21322417cdfa
26oglel <144705413+26oglel@users.noreply.github.com>
2023-09-26 10:53:17 -0400

    ahh shh pushit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 10.0f;
+    public float xRange;
+    public float yrange;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,12 +16,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
         Debug.Log("moveHorizontal:" + moveHorizontal);
 
-        float moveVertical = Input.GetAxis("Vertical");
+        float moveVertical = Input.GetAxisRaw("Vertical");
 
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        transform.Translate(movement * speed * Time.deltaTime);
 
         //   if (Input.GetKeyDown(KeyCode.D))
         //{ 
